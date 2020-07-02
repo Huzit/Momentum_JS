fbefbbcaa9b5d3ffcd82dad89e5190b3a2aa0063
Huzit <hwan4789@gmail.com>
2020-07-03 02:15:32 +0900

    'localStorage'

--- clock.js
+++ clock.js
@@ -1,17 +1,25 @@
-const clockContainer = document.querySelector(".js-clock"),
-  clockTitle = clockContainer.querySelector("h1");
+const clockContainer = document.querySelector(".js-clock"), //querySelector는 document에서 인치하는 문서내 첫 번째 Element를 반환한다. 콤마를 붙인건 클래스를 의미함
+  clockTitle = clockContainer.querySelector("h1"); //html 에서 div 태그로 그룹화 되있기 때문에 js-clock클래스에서 h1 Element를 불러와야 한다
 
+  //자바스크립트에서 외부 참조를 할 경우 (html, css,..etc) ""큰 따옴표를 붙여줘야 한다
+  //반면 내부 value일경우 그냥 써주면 된다
+
+  //시간을 보여주는 함수
 function getTime(){
-    const date = new Date();
-    const minute = date.getMinutes();
+    const date = new Date(); //Date객체를 생성
+    const minute = date.getMinutes(); 
     const hours = date.getHours();
     const second = date.getSeconds();
 
-    clockTitle.innerText = `${hours < 10 ? `0${hours}` : `${hours}`} : ${minute < 10 ? `0${minute}` : `${minute}`} : ${second < 10 ? `0${second}` : `${second}`}`
+    //시간, 분, 초를 가져와서 포맷에 맞게 h1 Element의 text에 할당한다.
+    //할당문이 경우에따라 복잡하게 바뀌니 주의깊게 봐야됨
+  
+    clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours} : ${minute < 10 ? `0${minute}` : minute} : ${second < 10 ? `0${second}` : second}`
 }
 function init(){
     getTime();
-    setInterval(getTime, 1000);
+    setInterval(getTime, 1000); //android 할 때도 써봤었지만 반복실행시켜주는 함수다 첫 번째 인수는 반복실행할 함수, 두번째는 밀리초단위 주기이다
 }
 
 init();
+//알아야 할것!! 기능별로 세분화 하자. 뭉탱이로 만들면 나중에 귀찮아진다. 분할 할 수 있을 떄 하자
---
+++ greeting.js
@@ -0,0 +1,28 @@
+const form = document.querySelector(".js-form"), //form 을 from이라고 적은 사람 대가리 박으시오
+    input = form.querySelector("input");
+    greeting = document.querySelector(".js-greetings");
+
+const USER_LS = "currentUser",
+    SHOWING_CN = "showing";
+
+function paintGreeting(text){ //이 부분은 input element에 적용되는 부분
+    form.classList.remove(SHOWING_CN); //form을 숨긴다.
+    greeting.classList.add(SHOWING_CN); //classList에 SHOWING_CN을 해줘야 화면에 나온다.
+    //classList가 무슨기능을 하는 놈인지를 자세히 알아보자
+    greeting.innerText = `Hello ${text}`; //.js-greetings 의 text를 변경
+}
+
+function loadName(){
+    const currentUser = localStorage.getItem(USER_LS); //localStorage에서 USER_LS를 가져옴
+    if(currentUser === null){   
+        console.log("null");
+    } else{
+        paintGreeting(currentUser);
+    }
+}
+
+function init(){
+    loadName();
+}
+
+init();
